1874eb2ca4f61069afac1604b5222b46391171ee
nicdkim <zzsehdrb@naver.com>
2024-11-04 23:35:36 +0900

    search

--- src/main/java/com/collawork/back/config/SecurityConfig.java
+++ src/main/java/com/collawork/back/config/SecurityConfig.java
@@ -51,6 +51,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/auth/social/**").permitAll()
                         .requestMatchers("/api/auth/login", "/api/auth/register", "/api/user/info").permitAll()
                         .requestMatchers("/login/oauth2/code/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/search").permitAll()
                         .anyRequest().authenticated())  // 나머지 경로는 인증 필요
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()));
 
